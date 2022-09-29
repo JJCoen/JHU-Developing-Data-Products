65aecb03a73bb729311734119c8e26ff94d0b27f
JJCoen <jim.coen@gmx.com>
2022-09-29 11:10:28 +0100

    added documentation

---
+++ 04_Web_App/loan-app/server.R
@@ -0,0 +1,33 @@
+# load Loans Data
+loan_data <- read.csv("./Data/loan_data_preproc.csv", stringsAsFactors = TRUE)
+setDT(loan_data)
+# remove index column inserted by Excel
+loan_data[, X := NULL]
+# Select required variables
+loan_data <- loan_data[, .(loan_status, loan_amnt, ir_cat, emp_cat)]
+# Convert response to categorical in order to 
+# identify names when plotting
+loan_data$loan_status <-ifelse(loan_data$loan_status ==1, 
+                               "Default!",
+                               "Loan Paid") %>% 
+  as.factor() %>% 
+  relevel(ref = "Loan Paid")
+
+function(input, output) {
+  
+  output$plot <- renderPlot({
+    # select interest rate and employment categories
+    subset_data <- loan_data[ ir_cat == input$ir &
+                                emp_cat == input$emp]
+    # create box plots
+    ggplot(subset_data, aes(y = loan_amnt, x = loan_status,
+                            color = loan_status) ) +
+      geom_boxplot() +
+      coord_flip() +
+      # Use brewer color palettes
+      scale_color_brewer(palette="Dark2") +
+      theme(legend.position="none") +
+      labs(title="Repayment",x="Loan Outcome", y = "Loan Amount") 
+    
+  }, res = 96)
+}
